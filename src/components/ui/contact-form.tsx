9cee53bf6619870bc0f1f53d8baa748ad79c3490
Oluwademilade Ala <demiladeala@gmail.com>
2025-08-14 23:07:23 +0100

    feat: add footer section

---
+++ src/components/ui/contact-form.tsx
@@ -0,0 +1,73 @@
+import Link from "next/link";
+import { Mail } from "../icons/mail";
+import { Comments } from "../icons/comments";
+
+export function ContactForm() {
+  return (
+    <form className="w-full lg:max-w-[392px] mx-auto p-6 bg-[#FCFCFC] rounded-lg">
+      <h2 className="text-xs mb-4 text-[#72D560]">FEEDBACK</h2>
+
+      <div className="text-2xl tracking-tighter">
+        <p className="text-[#222221] lg:w-[80%] mb-2">
+          Focus with accountability, Earn rewards,
+          <span className="text-[#A9A5B1]">
+            &nbsp; Level up your productivity
+          </span>{" "}
+        </p>
+      </div>
+      <div className="relative mt-4 mb-4 space-y-3">
+        {/* <label
+          className="block text-sm font-medium text-gray-700 mb-2"
+          htmlFor="email"
+        >
+          Email
+        </label> */}
+        <input
+          type="email"
+          id="email"
+          placeholder="Email"
+          className="w-full px-3 py-2 pr-10 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-[#72D560] text-sm placeholder:text-sm "
+          required
+        />
+        <div className="absolute top-[25%] right-3">
+          <Mail color="black" />
+        </div>
+      </div>
+      <div className="mb-4 relative ">
+        {/* <label
+          className="block text-sm font-medium text-gray-700 mb-2"
+          htmlFor="message"
+        >
+          Message
+        </label> */}
+        <textarea
+          id="message"
+          rows={3}
+          placeholder="Comments, Feedback, Suggestions..."
+          className="w-full px-3 py-2 pr-10 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-[#72D560] text-sm placeholder:text-sm resize-none"
+          required
+        ></textarea>
+
+        <div className="absolute top-[15%] right-3">
+          <Comments color="black" />
+        </div>
+      </div>
+      <button
+        type="submit"
+        className="w-full mt-3 bg-[#222221] text-white text-sm font-semibold py-3 hover:opacity-90 transition-all duration-200 cursor-pointer rounded-xl"
+      >
+        Send a request
+      </button>
+
+      <p className="mt-8">
+        <Link
+          target="_blank"
+          href="/#"
+          className="mt-8 text-[#222221] text-sm hover:underline"
+        >
+          Privacy
+        </Link>
+      </p>
+    </form>
+  );
+}
--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -1,10 +1,113 @@
+import Link from "next/link";
+import { ContactForm } from "./contact-form";
+import { Twitter } from "../icons/twitter";
+import { Youtube } from "../icons/youtube";
+import { Instagram } from "../icons/instagram";
+import { Facebook } from "../icons/facebook";
+
+const socialLinks = [
+  { name: "Facebook", url: "#", icon: Facebook },
+  { name: "Instagram", url: "#", icon: Instagram},
+  { name: "Youtube", url: "#", icon: Youtube },
+  { name: "Twitter", url: "#", icon: Twitter},
+];
+
 export function Footer() {
   return (
-    <footer className="mt-16 bg-[#72D560] py-8 md:py-12">
-      <div className="max-w-7xl mx-auto px-4">
-        <p className="text-center text-[#69666E] text-sm">
-          © {new Date().getFullYear()} LockedIn. All rights reserved.
-        </p>
+    <footer className="mt-16 bg-[#72D560] py-8 md:py-12 lg:py-16 px-5 md:px-16 lg:px-24">
+      <div className="w-full flex flex-col lg:flex-row gap-8 lg:gap-24">
+        <ContactForm />
+
+        <div className="w-full flex flex-col items-start justify-between">
+          <div className="w-full grid grid-cols-2 lg:grid-cols-3 gap-5 lg:gap-16">
+            <div>
+              <h3 className="text-[#FFC727] font-semibold mb-3 text-xs">
+                INFO
+              </h3>
+              <div className="flex flex-col space-y-2">
+                <Link
+                  href="#"
+                  className="transition-colors lg:hover:text-primaryPurple lg:hover:underline"
+                >
+                  Company
+                </Link>
+                <Link
+                  href="#"
+                  className="transition-colors lg:hover:text-primaryPurple lg:hover:underline"
+                >
+                  Products
+                </Link>
+                <Link
+                  href="#"
+                  className="transition-colors lg:hover:text-primaryPurple lg:hover:underline"
+                >
+                  Engineering
+                </Link>
+                <Link
+                  href="#"
+                  className="transition-colors lg:hover:text-primaryPurple lg:hover:underline"
+                >
+                  Services
+                </Link>
+                <Link
+                  href="#"
+                  className="transition-colors lg:hover:text-primaryPurple lg:hover:underline"
+                >
+                  Productions
+                </Link>
+              </div>
+            </div>
+
+            <div>
+              <h3 className="text-[#FFC727] font-semibold mb-3 text-xs uppercase">
+                Contact us
+              </h3>
+              <div className="flex flex-col space-y-2">
+                <Link
+                  href="#"
+                  className="transition-colors lg:hover:text-primaryPurple lg:hover:underline"
+                >
+                  +1 (999) 999-99-99
+                </Link>
+                <Link
+                  href="#"
+                  className="transition-colors lg:hover:text-primaryPurple lg:hover:underline"
+                >
+                  hello@logoipsum.com
+                </Link>
+                <Link
+                  href="#"
+                  className="transition-colors lg:hover:text-primaryPurple lg:hover:underline"
+                >
+                  London
+                </Link>
+              </div>
+            </div>
+
+            <div className="text-black text-lg lg:text-2xl font-semibold lg:ml-auto">LockedIn</div>
+          </div>
+
+          {/* SOCIALS */}
+          <div className="w-full flex items-center justify-between">
+            <div>
+              {socialLinks.map((link) => (
+                <Link
+                  key={link.name}
+                  href={link.url}
+                  className="inline-flex items-center p-3 mr-5 gap-2 text-xs border border-[#222221]/10 hover:bg-[#222221]/10 transition-all duration-300 rounded-full cursor-pointer"
+                >
+                  <link.icon color="#222221" fontSize={20} />
+                </Link>
+              ))}
+            </div>
+
+            <div>
+              <p className="text-xs text-[#151514] opacity-40">
+                LockedIN © 2025 — Copyright
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     </footer>
   );
